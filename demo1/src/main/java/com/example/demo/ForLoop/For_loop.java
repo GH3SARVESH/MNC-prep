a5c828343da7c35df00ad795e1d2a39a39fc4056
sarvesh.s <86788070+GH3SARVESH@users.noreply.github.com>
2026-05-11 09:26:10 +0530

    Add files via upload

---
+++ demo1/src/main/java/com/example/demo/ForLoop/For_loop.java
@@ -0,0 +1,41 @@
+package loops;
+
+public class For_loop {
+    public static void main(String[] args) {
+//        var sar = 6;
+//        for(int i =0  ; i <= sar; i++) {
+//            for (int j = i; j <= i; j ++){
+//               boolean sat = j % 2 == 0;
+//                System.out.println(sat );
+//        }
+//        }
+//reverse
+       // for(int i = 10 ;i >=1 ; i --){
+//            for(int i = 10; i >= 1; i--)
+           // System.out.println(i);
+   //     }
+
+
+//        Forloop with  divisible 2
+//        for(int i =2 ; i <= 20 ; i +=2 ) {
+//            System.out.println(i);
+//        }
+//        }
+
+        //sum of numbers
+//        int sum = 0;
+//        for(int i = 1; i <= 5; i++){
+//            sum +=  i;
+//        }
+//        System.out.println(sum);
+
+
+        //multiple of 5
+
+//        int n = 5;
+//
+//        for(int i = 1 ; i <= 10 ;i++){
+//            System.out.println(n + "x" + i+"="+(n*i));
+//        }
+    }
+}
