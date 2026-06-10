fb7016c39439dbf0c71db28d8d4bb08ebbf01955
sarvesh.s <86788070+GH3SARVESH@users.noreply.github.com>
2026-06-10 21:53:26 +0530

    Add files via upload
    csv file reader sample uploaded

---
+++ demo1/src/main/java/com/example/demo/demosarvesh/DemosarveshApplication.java
@@ -0,0 +1,13 @@
+package com.example.demosarvesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemosarveshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemosarveshApplication.class, args);
+	}
+
+}
