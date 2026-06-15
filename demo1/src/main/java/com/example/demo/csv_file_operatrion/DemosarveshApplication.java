03f7c5ef3ff963baa069b83397408c784679631b
sarvesh.s <86788070+GH3SARVESH@users.noreply.github.com>
2026-06-15 11:16:41 +0530

    Add files via upload

---
+++ demo1/src/main/java/com/example/demo/csv_file_operatrion/DemosarveshApplication.java
@@ -0,0 +1,13 @@
+package csv_file_operatrion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemosarveshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemosarveshApplication.class, args);
+	}
+
+}
---
+++ demo1/src/main/java/com/example/demo/csv_file_operatrion/csv_xl_workbook/csv_xl_wrokbook.java
@@ -0,0 +1,29 @@
+package csv_file_operatrion.csv_xl_workbook;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class csv_xl_wrokbook {
+    public static void main(String[] args) {
+
+        //to read csv
+
+//        String csvpath ="D:\\Project personal\\demosarvesh\\demosarvesh\\src\\main\\resources\\templates\\employee.csv";
+//        try(
+//                BufferedReader bf = new BufferedReader(new FileReader(csvpath))){
+//            String line;
+//            while((line = bf.readLine())!= null) {
+//                String[] data = line.split(",");
+//                System.out.println("id" + data[0]);
+//                System.out.println("name"+data[1]);
+//                System.out.println("salary"+data[2]);
+//                System.out.println("-------------------");
+//            }
+//
+//        } catch (
+//                IOException e) {
+//            e.printStackTrace();
+//        }
+    }
+}
